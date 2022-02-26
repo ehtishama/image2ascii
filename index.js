0f63d5e1c28cc657cf4ad1d9b353cdc6e6578fdb
ehtishama <ehtishamhassan9@gmail.com>
2022-02-26 10:12:25 +0500

    Refactor index.js

--- index.js
+++ index.js
@@ -1,85 +1,119 @@
-const numjs = require("numjs");
-const sharp = require("sharp");
 const fs = require("fs");
+const mathjs = require("mathjs");
+const sharp = require("sharp");
+const commandLineArgs = require("command-line-args");
+
+const optionDefinitions = [
+    { name: "src", type: String, alias: "s" },
+    { name: "out", type: String, alias: "o" },
+];
+
+const options = commandLineArgs(optionDefinitions);
+
+if (!options.src || !options.out) process.exit();
 
 async function main() {
-
-    let asciiPixels = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
-
-    const { data, info } = await sharp("images/profile.webp")
+    // reading image using sharpjs
+    const { data, info } = await sharp(options.src)
         .raw()
+        .resize({ width: 50, height: 50, fit: "contain" })
         .toBuffer({ resolveWithObject: true });
 
-    const pixelArray = new Uint8ClampedArray(data.buffer)
+    const pixelArray = new Uint8ClampedArray(data.buffer);
 
-    const ndarray = numjs.array([...pixelArray])
+    // reshape image array to a matrix using mathjs
+    let pixelMatrix = mathjs.reshape(
+        [...pixelArray],
+        [info.height, info.width, info.channels]
+    );
 
-    let pixelMatrix = ndarray.reshape(info.height, info.width, info.channels).tolist();
+    const brightnessMatrix = calculateBrightnessMatrix(pixelMatrix, info);
 
-    let brightnessMatrix = new Array(info.height);
+    const asciiMatrix = calculateAsciiMatrix(brightnessMatrix, info);
 
-    for (let i = 0; i < info.height; i++) {
+    printAsciiMatrix(asciiMatrix);
 
+
+    // fs.writeFile(options.out, output, err => {
+    //     if (err) console.log("Error while writing output to the file.");
+    // });
+
+    // fs.writeFile(options.o, outputInvert, err => {
+    //     if (err) console.log("Error while writing output to the file.");
+    // });
+}
+
+
+main();
+
+function scale(x) {
+    return Math.round(x / 4);
+}
+
+function calculateBrightnessMatrix(
+    pixelMatrix,
+    options = {
+        height: pixelMatrix.length,
+        width: pixelMatrix[0].length,
+        channels: 3,
+    }
+) {
+    const { height, width, channels } = options;
+
+    let brightnessMatrix = new Array(height);
+
+    for (let i = 0; i < height; i++) {
         let row = [];
-        for (let j = 0; j < info.width; j++) {
-            row.push(Math.round(pixelMatrix[i][j].reduce((a, b) => a + b, 0) / 3))
+        for (let j = 0; j < width; j++) {
+            row.push(
+                Math.round(pixelMatrix[i][j].reduce((a, b) => a + b, 0) / channels)
+            );
         }
         brightnessMatrix[i] = row;
-
     }
+    return brightnessMatrix;
+}
 
-    let asciiMatrix = new Array(info.height);
+function calculateAsciiMatrix(
+    brightnessMatrix,
+    options = {
+        height: brightnessMatrix.length,
+        width: brightnessMatrix[0].width,
+    }
+) {
+    let asciiPixels =
+        '`^",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$';
 
-    for (let i = 0; i < info.height; i++) {
+    const { height, width } = options;
+
+    let asciiMatrix = new Array(height);
+
+    for (let i = 0; i < height; i++) {
         let row = [];
-        for (let j = 0; j < info.width; j++) {
-
-            let ascii_idx = linearScale(brightnessMatrix[i][j])
+        for (let j = 0; j < width; j++) {
+            let ascii_idx = scale(brightnessMatrix[i][j]);
             row.push(asciiPixels[ascii_idx]);
         }
         asciiMatrix[i] = row;
     }
 
-    let asciiMatrixInverted = new Array(info.height)
+    return asciiMatrix;
+}
 
-    for (let i = 0; i < info.height; i++) {
-        let row = [];
-        for (let j = 0; j < info.width; j++) {
+function printAsciiMatrix(asciiMatrix) {
+    let output;
 
-            let ascii_idx = linearScale(255 - brightnessMatrix[i][j])
-            row.push(asciiPixels[ascii_idx]);
-        }
-        asciiMatrixInverted[i] = row;
-    }
-
-    let output, outputInvert
-
-    output = outputInvert = `<p style="font-family: monospace; font-size: 6px;">`
+    output = "";
 
     // write to a text file
-    for (let i = 0; i < info.height; i++) {
-        output = output.concat(asciiMatrix[i].map(asciiPixel => asciiPixel.repeat(2)).join('').concat("<br>"));
-
-        outputInvert = outputInvert.concat(asciiMatrixInverted[i].map(asciiPixel => asciiPixel.repeat(2)).join('').concat("<br>"))
+    for (let i = 0; i < asciiMatrix.length; i++) {
+        output = output.concat(
+            asciiMatrix[i]
+                .map((asciiPixel) => asciiPixel.repeat(2))
+                .join("")
+                .concat("\n")
+        );
     }
 
-    output = output.concat("</p>");
-    outputInvert  = outputInvert.concat("</p>")
-
-    fs.writeFile("output.html", output, err => {
-        if (err) console.log("Error while writing output to the file.");
-    });
-
-    fs.writeFile("output-inverted.html", outputInvert, err => {
-        if (err) console.log("Error while writing output to the file.");
-    });
-
-
+    console.log(output);
 }
-
-function linearScale(x) {
-
-    return Math.round(x / 4);
-}
-
-main()
